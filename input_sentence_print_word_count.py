2c4806f12cb39cf8c8da3d8c65e720bb2236ac31
althmrll <altheamariellcdelara@iskolarngbayan.pup.edu.ph>
2025-03-19 19:16:23 +0800

    added variable to try and make the word counter work

--- input_sentence_print_word_count.py
+++ input_sentence_print_word_count.py
@@ -1,6 +1,6 @@
 #Ask user to input sentence
-sentence= str(input("Enter a sentence:"))
+sentence= (input("Enter a sentence:"))
 
 #count number of wordssentence.split()
-word= sentence.split()
-print("there are",word.count(sentence), "words in the sentence you inputted.")
+word= str(sentence.split())
+print("there are",word.count(word), "words in the sentence you inputted.")
