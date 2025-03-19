7144e11184e98f02e3e9f22bc75d953bfde8fe04
althmrll <altheamariellcdelara@iskolarngbayan.pup.edu.ph>
2025-03-19 18:03:06 +0800

    removed list and tried using split function

--- input_sentence_print_word_count.py
+++ input_sentence_print_word_count.py
@@ -1,10 +1,6 @@
-words=[]
-
 #Ask user to input sentence
 sentence= input("Enter a sentence:")
 
-#Add words in the list
-words.append(sentence)
-
 #count number of words
-print("there are", words.count(), "words in the sentence you inputted.")
+sentence.split()
+print("there are", sentence.count(sentence), "words in the sentence you inputted.")
