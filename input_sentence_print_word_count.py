bef487d225e445cc25ded2a9ddbf1bbdf9f4d546
althmrll <altheamariellcdelara@iskolarngbayan.pup.edu.ph>
2025-03-19 17:58:07 +0800

    tried using list to see if it fixes the error (trial 1)

--- input_sentence_print_word_count.py
+++ input_sentence_print_word_count.py
@@ -1,5 +1,10 @@
+words=[]
+
 #Ask user to input sentence
 sentence= input("Enter a sentence:")
 
+#Add words in the list
+words.append(sentence)
+
 #count number of words
-print("there are", sentence.count(), "words in the sentence you inputted.")
+print("there are", words.count()), "words in the sentence you inputted.")
