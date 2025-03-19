17934daa59f5b1f5613ee303425e9b8cf024386a
althmrll <altheamariellcdelara@iskolarngbayan.pup.edu.ph>
2025-03-19 17:23:07 +0800

    editted input message so it is specified that a full name should be entered

--- input_fullname_with_space_in_beginning_print_without_the_space.py
+++ input_fullname_with_space_in_beginning_print_without_the_space.py
@@ -1,5 +1,5 @@
 # Ask user to input their name (with hefty amount of space)
-name=input("Add a large space and Enter your name:")
+name=input("Add a large space and Enter your full name:")
 
 # Print their name without the spaces
 print(name.strip())
